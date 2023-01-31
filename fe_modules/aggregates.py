7fa7d5e538a143e7a5a2d674947f68dcb1b43bad
Lev Novitskiy <levnovitskiy@gmail.com>
2023-01-31 22:56:58 +1000

    Feature engeneering module added
    * Basic aggregations
    * Cat feature addition

---
+++ fe_modules/aggregates.py
@@ -0,0 +1,27 @@
+import polars as pl
+
+
+def get_agg_sum(df: pl.DataFrame, target_col: str, agg_col: str = "user_id") -> pl.DataFrame:
+    return df.groupby(agg_col).agg(pl.col(target_col).sum().alias(f'{agg_col}_sum'))
+
+
+def get_agg_mean(df: pl.DataFrame, target_col: str, agg_col: str = "user_id") -> pl.DataFrame:
+    return df.groupby(agg_col).agg(pl.col(target_col).mean().alias(f'{agg_col}_mean'))
+
+
+def get_agg_mode(df: pl.DataFrame, target_col: str, agg_col: str = "user_id") -> pl.DataFrame:
+    return df.groupby(agg_col).agg(pl.col(target_col).mode().alias(f'{agg_col}_mean'))
+
+
+def get_agg_median(df: pl.DataFrame, target_col: str, agg_col: str = "user_id") -> pl.DataFrame:
+    return df.groupby(agg_col).agg(pl.col(target_col).median().alias(f'{agg_col}_median'))
+
+
+def get_agg_std(df: pl.DataFrame, target_col: str, agg_col: str = "user_id") -> pl.DataFrame:
+    return df.groupby(agg_col).agg(pl.col(target_col).std().alias(f'{agg_col}_std'))
+
+
+def get_agg_n_unique(df: pl.DataFrame, target_col: str, agg_col: str = "user_id") -> pl.DataFrame:
+    return df.groupby(agg_col).agg(pl.col(target_col).n_unique().alias(f'{target_col}_n_unique'))
+
+
---
+++ fe_modules/feature_merges.py
@@ -0,0 +1,13 @@
+import polars as pl
+
+
+def add_cat_features(df: pl.DataFrame, feature_a: str, feature_b: str, alias: str) -> pl.Series:
+    new_feature = df[feature_a] + "_" + df[feature_b]
+
+    if alias:
+        new_feature = new_feature.alias(alias)
+    else:
+        new_feature = new_feature.alias(f"{feature_a}_{feature_b}")
+
+    return new_feature
+
