221dc364edeeab197fef80343273cfaaba6f26ab
sheep-shaun <egor.kashpar.03@mail.ru>
2023-02-06 18:29:51 +0300

    fixes

--- fe_modules/cyclical_features.py
+++ fe_modules/cyclical_features.py
@@ -1,6 +1,10 @@
-def generate_cyclical_features(df, col_name, period, start_num=0):
-    kwargs = {
-        f'sin_{col_name}' : lambda x: np.sin(2 * np.pi * (df[col_name]-start_num) / period),
-        f'cos_{col_name}' : lambda x: np.cos(2 * np.pi * (df[col_name]-start_num) / period)
-             }
-    return df.assign(**kwargs)
+import numpy as np
+import polars as pl
+
+
+def generate_cyclical_features(df: pl.DataFrame, col_name: str, period: int, start_num: int = 0) -> pl.DataFrame:
+    sin_generator = lambda x: np.sin(2 * np.pi * (df[col_name] - start_num) / period)
+    cos_generator = lambda x: np.cos(2 * np.pi * (df[col_name] - start_num) / period)
+    df = df.with_column(pl.col(col_name).apply(sin_generator).alias(f"sin_{col_name}"))
+    df = df.with_column(pl.col(col_name).apply(cos_generator).alias(f"cos_{col_name}"))
+    return df
