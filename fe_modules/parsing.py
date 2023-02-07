4d35f38c30417e66f8009ee98b59a1d302661d08
fedor <fedor.avilov@yandex.ru>
2023-02-07 19:37:10 +0300

    Parsing via dask & bs all_urls not cleared cities csv

--- fe_modules/parsing.py
+++ fe_modules/parsing.py
@@ -1,17 +1,47 @@
+from ctypes import c_char_p
 import requests
 from bs4 import BeautifulSoup
-from tqdm import tqdm
+import multiprocessing
+import time
+import pandas as pd
+import dask.dataframe as dd
+from dask.diagnostics import ProgressBar
 
 
-def parse_raw_texts(urls: list):
-    texts = []
-
-    for url in tqdm(urls):
+def parse_bs(url: str, text: str):
+    try:
         res = requests.get(url)
-        html_page = res.content
-        soup = BeautifulSoup(html_page, 'html.parser')
-        text = soup.find_all(text=True)
+        soup = BeautifulSoup(res.content, "html.parser")
+        for data in soup(['style', 'script']):
+            data.decompose()
+        text.value = ' '.join(soup.stripped_strings)
+    except requests.exceptions.ConnectionError:
+        text.value = "NULL"
 
-        texts.append(text)
 
-    return texts
+def parse_raw_texts(url):
+    url = url['url_host']
+    manager = multiprocessing.Manager()
+    text = manager.Value(c_char_p, "NULL")
+    text.value = "NULL"
+    p = multiprocessing.Process(target=parse_bs, name="Foo", args=(url, text,))
+    p.start()
+    time.sleep(1)
+    p.terminate()
+    p.join()
+
+    return text.value
+
+
+def parse(sites_path: str, out_path: str):
+    df = pd.read_csv(sites_path)
+    df.url_host = 'http://' + df.url_host
+    df['text'] = [''] * df.shape[0]
+    ddf = dd.from_pandas(df, npartitions=8)
+    res = ddf.apply(parse_raw_texts, axis=1, meta=('text', 'object'))
+    with ProgressBar():
+        res.compute()
+
+    df.to_csv(out_path)
+
+
---
+++ parse.py
@@ -0,0 +1,5 @@
+from fe_modules.parsing import parse
+
+if __name__ == "__main__":
+    parse('C:\\Users\\fedor\\Downloads\\mlcup\\mts-ml-cup\\all_urls.csv',
+          'C:\\Users\\fedor\\Downloads\\mlcup\\mts-ml-cup\\sites_out.csv')
