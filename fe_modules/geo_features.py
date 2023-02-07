93479236009ce714da84ffc16f22aa3ac494b412
RussianFreak <alapteff@mail.ru>
2023-02-07 19:22:33 +0300

    Adding geo_features

---
+++ fe_modules/geo_features.py
@@ -0,0 +1,13 @@
+from aggregates import get_agg_mean, get_agg_min, get_agg_max
+import polars as ps
+def mean_first_visit(df: pl.DataFrame) -> pl.DataFrame:
+    first_visit = get_agg_min(df, agg_col=['user_id', 'date'], alias='first_visit')
+    mean_fv = get_agg_mean(first_visit, 'mean_fv')
+    del mean_fv['first_visit']
+    return mean_fv
+
+def mean_last_visit(df: pl.DataFrame) -> pl.DataFrame:
+    last_visit = get_agg_max(df, agg_col=['user_id', 'date'], alias='last_visit')
+    mean_lv = get_agg_mean(last_visit, 'mean_lv')
+    del mean_lv['last_visit']
+    return mean_lv
