9fed88c09bf6472d2259e74adeb07469071c080d
sheep-shaun <egor.kashpar.03@mail.ru>
2023-02-07 20:50:15 +0300

    datetime.py rewritten to pandas

--- fe_modules/datetime_features.py
+++ fe_modules/datetime_features.py
@@ -1,10 +1,8 @@
-from datetime import timedelta
-
 import pandas as pd
 import polars as pl
 
 
-def get_year(df: pd.DataFrame, date_col: str = "year") -> pd.DataFrame:
+def get_year(df: pd.DataFrame, date_col: str = "date") -> pd.DataFrame:
     df["year"] = pd.DatetimeIndex(df[date_col]).year
     return df
 
@@ -14,13 +12,14 @@ def get_month(df: pd.DataFrame, date_col: str = "date") -> pd.DataFrame:
     return df
 
 
-def get_day(df: pd.DataFrame, date_col: str = "day") -> pd.DataFrame:
-    df["dat"] = pd.DatetimeIndex(df[date_col]).day
+def get_day(df: pd.DataFrame, date_col: str = "date") -> pd.DataFrame:
+    df["day"] = pd.DatetimeIndex(df[date_col]).day
     return df
 
 
-def get_timestamp(df: pl.DataFrame, date_col: str = "date", scaler: int = 1e8) -> pl.DataFrame:
-    return df.with_column((pl.col(date_col).dt.timestamp() /  scaler).alias("timestamp"))
+def get_timestamp(df: pd.DataFrame, date_col: str = "date", scaler: int = 10e9, alias: str = "timestamp") -> pd.DataFrame:
+    df[alias] = pd.DatetimeIndex(df[date_col]).astype(int) / scaler
+    return df
 
 
 def part_of_day_to_hour(df: pl.DataFrame, col: str = "part_of_day", alias: str = "hour") -> pl.DataFrame:
