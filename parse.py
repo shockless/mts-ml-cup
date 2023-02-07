b45af111d2640da27deb8ae42b875a6131802c16
fedor <fedor.avilov@yandex.ru>
2023-02-07 21:57:14 +0300

    parsing update working now ^)

--- parse.py
+++ parse.py
@@ -2,5 +2,5 @@ from fe_modules.parsing import parse
 import os
 
 if __name__ == "__main__":
-    parse(os.path.join(os.path.dirname(__file__), '\\all_urls.csv'),
-          os.path.join(os.path.dirname(__file__), '\\sites_out.csv'))
+    parse(os.path.join(os.path.abspath(os.getcwd()), '10k_most_freq.csv'),
+          os.path.join(os.path.abspath(os.getcwd()), 'sites_out.csv'))
