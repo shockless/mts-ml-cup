00c040458fa08c91b47388140348fb11630c8ff1
RussianFreak <alapteff@mail.ru>
2023-02-08 19:41:21 +0300

    fix smt

--- fe_modules/geo_features.py
+++ fe_modules/geo_features.py
@@ -10,7 +10,7 @@ def mean_first_visit(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def mean_last_visit(df: pd.DataFrame) -> pd.DataFrame:
-    last_visit = get_agg_min(df, agg_col=['user_id', 'date'], col='last_visit', target_col='part_of_day')
+    last_visit = get_agg_max(df, agg_col=['user_id', 'date'], col='last_visit', target_col='part_of_day')
     mean_fv = get_agg_mean(last_visit, agg_col=['user_id', 'date'], col='mean_fv', target_col='last_visit')
     del mean_fv['last_visit']
     return mean_fv
