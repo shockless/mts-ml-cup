425a61cc568a2916ca80018c45cc3e2091cba5dd
sheep-shaun <egor.kashpar.03@mail.ru>
2023-02-16 00:50:03 +0300

    add top_n_mode

--- fe_modules/aggregates.py
+++ fe_modules/aggregates.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def get_agg_count(df: pd.DataFrame,
@@ -30,24 +31,6 @@ def get_agg_sum(df: pd.DataFrame,
         col_name = f'{agg_col}_group_sum'
 
     agg = df.groupby([agg_col])[target_col].sum().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
-    if sort:
-        return df.sort_values(by=agg_col)
-
-    return df
-
-
-def get_agg_mode(df: pd.DataFrame,
-                 agg_col: str = "user_id",
-                 target_col: str = None,
-                 alias: str = None,
-                 sort: bool = False) -> pd.DataFrame:
-    if alias:
-        col_name = alias
-    else:
-        col_name = f'{agg_col}_group_mode'
-
-    agg = df.groupby([agg_col])[target_col].mode().to_frame().rename(columns={target_col: col_name}).reset_index()
     df = df.merge(agg, how="left", on=[agg_col])
     if sort:
         return df.sort_values(by=agg_col)
@@ -179,4 +162,29 @@ def get_price_of_all_cpes(df: pd.DataFrame,
     if sort:
         return df.sort_values(by=agg_col)
 
+    return df
+
+
+def get_top_n_mode(df: pd.DataFrame,
+                   agg_col: str = "user_id",
+                   target_col: str = None,
+                   n: int = 1,
+                   alias: str = None) -> pd.DataFrame:
+    if alias:
+        col_name = alias
+    else:
+        col_name = f'{agg_col}_mode'
+
+    def n_mode(s):
+        temp = s.value_counts()[:n].index
+        temp = np.append(temp, np.array(["<blank>"] * (n - len(temp))))
+        return temp
+
+    agg = df.groupby([agg_col])[target_col].agg(n_mode).to_frame()
+
+    agg = pd.DataFrame(np.concatenate((np.expand_dims(agg.index.to_numpy().astype(object), axis=1),
+                                       np.stack(agg[target_col].values)), axis=1),
+                       columns=[agg_col] + [f"{col_name}_{i}" for i in range(n)])
+
+    df = df.merge(agg, how="left", on=[agg_col])
     return df
