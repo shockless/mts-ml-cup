9f096419f1ccb51384df7d8576b8c2e618206dfe
RussianFreak <alapteff@mail.ru>
2023-02-17 01:09:39 +0300

    fixed cb_pipeline_v01

--- fe_modules/predictability_v01.py
+++ fe_modules/predictability_v01.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
 from catboost import CatBoostRegressor
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedGroupKFold
 
 RANDOM_STATE = 42
 DEFAULT_PARAMS = {
@@ -10,14 +11,14 @@ DEFAULT_PARAMS = {
 }
 
 
-def cb_pipeline(df: pd.DataFrame, target_feature: str, new_feature: str = None,
-                cb_params: dict = DEFAULT_PARAMS, n_folds=5) -> pd.DataFrame:
+def cb_predictability(df: pd.DataFrame, target_feature: str, new_feature: str = None,
+                      cb_params: dict = DEFAULT_PARAMS, n_folds=5) -> pd.DataFrame:
     y = df[target_feature]
-    X = df.drop(columns=[target_feature])
-    preds = y.copy()
-    kf = KFold(n_splits=n_folds, random_state=RANDOM_STATE, shuffle=True)
-    i = 0
-    for train_index, val_index in kf.split(X):
+    users = df['user_id']
+    X = df.drop(columns=[target_feature, 'user_id'])
+    preds = np.zeros(shape=y.shape)
+    sgkf = StratifiedGroupKFold(n_splits=n_folds, random_state=RANDOM_STATE, shuffle=True)
+    for i, (train_index, val_index) in enumerate(sgkf.split(X, y, groups=users)):
         X_train, X_val = X.iloc[train_index], X.iloc[val_index]
         y_train, y_val = y.iloc[train_index], y.iloc[val_index]
         model = CatBoostRegressor(**cb_params)
@@ -29,8 +30,8 @@ def cb_pipeline(df: pd.DataFrame, target_feature: str, new_feature: str = None,
                          format='cbm')
         i += 1
     if new_feature is None:
-        feat_name = target_feature + '_preds'
+        feat_name = target_feature + '_predictability'
     else:
         feat_name = new_feature
-    df[feat_name] = abs(df[target_feature] - preds)
+    df[feat_name] = abs(y - preds)
     return df
