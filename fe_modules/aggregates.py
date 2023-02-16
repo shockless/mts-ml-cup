bf0b10bc7bd5ac87bec880f902be64efa0f9af2a
sheep-shaun <egor.kashpar.03@mail.ru>
2023-02-17 01:42:13 +0300

    fixes

--- fe_modules/aggregates.py
+++ fe_modules/aggregates.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 
+from typing import Union
+
 
 def get_agg_count(df: pd.DataFrame,
-                  agg_col: str = "user_id",
+                  agg_col: Union[str, list] = "user_id",
                   target_col: str = None,
                   alias: str = None,
                   sort: bool = False) -> pd.DataFrame:
@@ -12,8 +14,11 @@ def get_agg_count(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_count'
 
-    agg = df.groupby([agg_col])[target_col].count().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].count().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -21,7 +26,7 @@ def get_agg_count(df: pd.DataFrame,
 
 
 def get_agg_sum(df: pd.DataFrame,
-                agg_col: str = "user_id",
+                agg_col: Union[str, list] = "user_id",
                 target_col: str = None,
                 alias: str = None,
                 sort: bool = False) -> pd.DataFrame:
@@ -30,8 +35,11 @@ def get_agg_sum(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_sum'
 
-    agg = df.groupby([agg_col])[target_col].sum().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].sum().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -39,7 +47,7 @@ def get_agg_sum(df: pd.DataFrame,
 
 
 def get_agg_mean(df: pd.DataFrame,
-                 agg_col: str = "user_id",
+                 agg_col: Union[str, list] = "user_id",
                  target_col: str = None,
                  alias: str = None,
                  sort: bool = False) -> pd.DataFrame:
@@ -48,8 +56,11 @@ def get_agg_mean(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_mean'
 
-    agg = df.groupby([agg_col])[target_col].mean().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].mean().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -57,7 +68,7 @@ def get_agg_mean(df: pd.DataFrame,
 
 
 def get_agg_max(df: pd.DataFrame,
-                agg_col: str = "user_id",
+                agg_col: Union[str, list] = "user_id",
                 target_col: str = None,
                 alias: str = None,
                 sort: bool = False) -> pd.DataFrame:
@@ -66,8 +77,11 @@ def get_agg_max(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_max'
 
-    agg = df.groupby([agg_col])[target_col].max().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].max().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -75,7 +89,7 @@ def get_agg_max(df: pd.DataFrame,
 
 
 def get_agg_min(df: pd.DataFrame,
-                agg_col: str = "user_id",
+                agg_col: Union[str, list] = "user_id",
                 target_col: str = None,
                 alias: str = None,
                 sort: bool = False) -> pd.DataFrame:
@@ -84,8 +98,11 @@ def get_agg_min(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_max'
 
-    agg = df.groupby([agg_col])[target_col].min().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].min().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -93,7 +110,7 @@ def get_agg_min(df: pd.DataFrame,
 
 
 def get_agg_median(df: pd.DataFrame,
-                   agg_col: str = "user_id",
+                   agg_col: Union[str, list] = "user_id",
                    target_col: str = None,
                    alias: str = None,
                    sort: bool = False) -> pd.DataFrame:
@@ -102,8 +119,11 @@ def get_agg_median(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_median'
 
-    agg = df.groupby([agg_col])[target_col].median().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].median().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -111,7 +131,7 @@ def get_agg_median(df: pd.DataFrame,
 
 
 def get_agg_std(df: pd.DataFrame,
-                agg_col: str = "user_id",
+                agg_col: Union[str, list] = "user_id",
                 target_col: str = None,
                 alias: str = None,
                 sort: bool = False) -> pd.DataFrame:
@@ -120,8 +140,11 @@ def get_agg_std(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_std'
 
-    agg = df.groupby([agg_col])[target_col].std().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].std().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -129,7 +152,7 @@ def get_agg_std(df: pd.DataFrame,
 
 
 def get_agg_nunique(df: pd.DataFrame,
-                    agg_col: str = "user_id",
+                    agg_col: Union[str, list] = "user_id",
                     target_col: str = None,
                     alias: str = None,
                     sort: bool = False) -> pd.DataFrame:
@@ -138,8 +161,11 @@ def get_agg_nunique(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_nunique'
 
-    agg = df.groupby([agg_col])[target_col].nunique().to_frame().rename(columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].nunique().to_frame().rename(columns={target_col: col_name}).reset_index()
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -147,7 +173,7 @@ def get_agg_nunique(df: pd.DataFrame,
 
 
 def get_price_of_all_cpes(df: pd.DataFrame,
-                          agg_col: str = "user_id",
+                          agg_col: Union[str, list] = "user_id",
                           target_col: str = None,
                           alias: str = None,
                           sort: bool = False) -> pd.DataFrame:
@@ -156,9 +182,12 @@ def get_price_of_all_cpes(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_group_price_of_all_cpes'
 
-    agg = df.groupby([agg_col])[target_col].unique().sum().to_frame().rename(
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
+    agg = df.groupby(agg_col)[target_col].unique().sum().to_frame().rename(
         columns={target_col: col_name}).reset_index()
-    df = df.merge(agg, how="left", on=[agg_col])
+    df = df.merge(agg, how="left", on=agg_col)
     if sort:
         return df.sort_values(by=agg_col)
 
@@ -166,7 +195,7 @@ def get_price_of_all_cpes(df: pd.DataFrame,
 
 
 def get_top_n_mode(df: pd.DataFrame,
-                   agg_col: str = "user_id",
+                   agg_col: Union[str, list] = "user_id",
                    target_col: str = None,
                    n: int = 1,
                    alias: str = None) -> pd.DataFrame:
@@ -175,16 +204,19 @@ def get_top_n_mode(df: pd.DataFrame,
     else:
         col_name = f'{agg_col}_mode'
 
+    if isinstance(agg_col, str):
+        agg_col = [agg_col]
+
     def n_mode(s):
         temp = s.value_counts()[:n].index
         temp = np.append(temp, np.array(["<blank>"] * (n - len(temp))))
         return temp
 
-    agg = df.groupby([agg_col])[target_col].agg(n_mode).to_frame()
+    agg = df.groupby(agg_col)[target_col].agg(n_mode).to_frame()
 
     agg = pd.DataFrame(np.concatenate((np.expand_dims(agg.index.to_numpy().astype(object), axis=1),
                                        np.stack(agg[target_col].values)), axis=1),
-                       columns=[agg_col] + [f"{col_name}_{i}" for i in range(n)])
+                       columns=agg_col + [f"{col_name}_{i}" for i in range(n)])
 
-    df = df.merge(agg, how="left", on=[agg_col])
+    df = df.merge(agg, how="left", on=agg_col)
     return df
--- fe_modules/geo_features.py
+++ fe_modules/geo_features.py
@@ -1,4 +1,3 @@
-from fe_modules.aggregates import get_agg_mean, get_agg_min, get_agg_max
 import pandas as pd
 from modules.memory_utils import pandas_reduce_mem_usage
 from geopy.distance import geodesic
@@ -18,20 +17,6 @@ COL_NAMES = dict(
     lat="latitude",
     lon="longitude",
 )
-
-
-def mean_first_visit(df: pd.DataFrame) -> pd.DataFrame:
-    first_visit = get_agg_min(df, agg_col=['user_id', 'date'], col='first_visit', target_col='part_of_day')
-    mean_fv = get_agg_mean(first_visit, agg_col=['user_id', 'date'], col='mean_fv', target_col='first_visit')
-    del mean_fv['first_visit']
-    return mean_fv
-
-
-def mean_last_visit(df: pd.DataFrame) -> pd.DataFrame:
-    last_visit = get_agg_max(df, agg_col=['user_id', 'date'], col='last_visit', target_col='part_of_day')
-    mean_lv = get_agg_mean(last_visit, agg_col=['user_id', 'date'], col='mean_fv', target_col='last_visit')
-    del mean_lv['last_visit']
-    return mean_lv
 
 
 def process_utc(cities: pd.DataFrame, timezone_col: str = "timezone"):
