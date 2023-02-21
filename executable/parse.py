8afc4b0ece6f516ae0fd652818f522ddd8322c3f
fedor <fedor.avilov@yandex.ru>
2023-02-21 21:54:53 +0300

    - parsing encoding update

--- executable/parse.py
+++ executable/parse.py
@@ -2,6 +2,6 @@ from fe_modules.parsing import parser
 import os
 
 if __name__ == "__main__":
-    parser().parse(os.path.join(os.path.abspath(os.getcwd()), '../hh_nlu.csv'),
-                   os.path.join(os.path.abspath(os.getcwd()), '../hh_cs.xls'),
-                   n_partitions=8, timeout=10, max_retries=1 )
+    parser().parse('/home/fedor/PycharmProjects/mts-ml-cup/executable/to_check.csv',
+                   os.path.join(os.path.abspath(os.getcwd()), './hh_cs.xls'),
+                   n_partitions=8, timeout=10, max_retries=1)
