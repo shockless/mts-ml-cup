6a466d8d14b2bd369e8037d67078c0817a7a21f0
sheep-shaun <egor.kashpar.03@mail.ru>
2023-02-28 23:56:44 +0300

    zero_shot updated

--- fe_modules/zero_shot.py
+++ fe_modules/zero_shot.py
@@ -1,17 +1,51 @@
 from transformers import pipeline
 
+import pandas as pd
+from torch.utils.data import DataLoader
+
+from tqdm.auto import tqdm
+
 import torch
 
 
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, texts, labels):
+        self.labels = labels
+        self.texts = texts
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, index):
+        X = self.texts[index]
+        y = self.labels[index]
+        return X, y
+
+
 class ZeroShot:
-    def __init__(self, model_name: str = "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", device: str = "cuda"):
+    def __init__(self, model_name: str = "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", device: str = "cuda:0"):
         self.device = device
-        # self.model = pipeline("zero-shot-classification", model=model_name, device=torch.device(self.device))
-        self.model = pipeline("zero-shot-classification", model=model_name)
+        self.model = pipeline("zero-shot-classification", model=model_name, device=torch.device(self.device))
 
     def predict_proba(self, texts: list, labels: list, batch_size: int = 128, multi_label: bool = False):
-        output = self.model(texts, labels, multi_label=multi_label)
-        return output
+        dataloader = DataLoader(texts, batch_size=batch_size, shuffle=False)
+
+        outputs = list()
+
+        for batch in tqdm(dataloader):
+            output = self.model(batch, labels, multi_label=multi_label)
+            for i in range(len(output)):
+                output[i] = {"labels": output[i]["labels"], "scores": output[i]["scores"]}
+                example = list(zip(output[i]["labels"], output[i]["scores"]))
+                example.sort(key=lambda x: x[0])
+                example = [scores[1] for scores in example]
+                outputs.append(example)
+
+        labels.sort()
+
+        df = pd.DataFrame(data=outputs, columns=labels)
+
+        return df
 
     def __call__(self, texts: list, labels: list, batch_size: int = 128, multi_label: bool = False):
         return self.predict_proba(texts, labels, batch_size, multi_label)
@@ -25,7 +59,7 @@ class ZeroShot:
 #
 #
 # class ZeroShot:
-#     def __init__(self, model_name: str = "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", device: str = "cuda"):
+#     def __init__(self, model_name: str = "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli", device: str = "cuda:0"):
 #         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 #         self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
 #         self.device = device
@@ -34,7 +68,7 @@ class ZeroShot:
 #
 #     def predict_proba(self, texts: list, labels: list, batch_size: int = 128,
 #                       multi_label: bool = False) -> torch.Tensor:
-#         tokenized_inputs = self.tokenizer(texts, labels, truncation=True, padding=True, return_tensors="pt")
+#         tokenized_inputs = self.tokenizer([texts, labels], truncation=True, padding=True, return_tensors="pt")
 #         input_ids = tokenized_inputs["input_ids"]
 #
 #         dataset = TensorDataset(input_ids)
@@ -47,7 +81,7 @@ class ZeroShot:
 #             b_inputs = b_inputs.to(self.device)
 #
 #             with torch.inference_mode():
-#                 output = self.model(b_inputs, multi_label=multi_label)
+#                 output = self.model(b_inputs)
 #                 prediction = torch.softmax(output["logits"][0], -1).tolist()
 #                 outputs.append(prediction)
 #
